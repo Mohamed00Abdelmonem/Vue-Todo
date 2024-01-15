f67b71418a3370e7abc00a9af6e34986bfda0b77
unknown <mmohamedabdelm@gmail.com>
2024-01-15 20:25:35 +0200

    istall font-awesome and add table for taskes

--- src/components/todoApp.vue
+++ src/components/todoApp.vue
@@ -5,6 +5,38 @@
         <input type="text" placeholder="Enter Task" class="w-100 form-control">
         <button class="ms-2 btn btn-warning">add</button>
       </div>
+
+      <table class="table mt-5 table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">Task</th>
+              <th scope="col">Status</th>
+              <th scope="col">Edit</th>
+              <th scope="col">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Study js</th>
+              <td>todo</td>
+              <td>
+                <div>
+                <span class="fa fa-pen"></span>
+              </div>
+            </td>
+              <td> 
+                 <div>
+                  <span class="fa fa-trash"></span>
+                </div>
+            </td>
+            </tr>
+           
+          </tbody>
+      </table>
+
+
+
+
   </div> 
 </template>
 
