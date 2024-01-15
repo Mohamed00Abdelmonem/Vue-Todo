e97d1339513217407b7b717db6a3c83ff879297d
unknown <mmohamedabdelm@gmail.com>
2024-01-15 20:07:56 +0200

    starting in app todoApp and install bootstrap in vue

---
+++ src/components/todoApp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+      <h2 class="text-center mt-5">ToDo App</h2>
+      <div class="d-flex mt-5">
+        <input type="text" placeholder="Enter Task" class="w-100 form-control">
+        <button class="ms-2 btn btn-warning">add</button>
+      </div>
+  </div> 
+</template>
+
+<script>
+export default {
+  name: 'todoApp'
+}
+</script>
+
+<style scoped>
+/* Add your component-specific styles here */
+</style>
